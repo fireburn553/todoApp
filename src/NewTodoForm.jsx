4887675635ce50523340e7225148d2557cb424e6
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-06-08 23:35:13 +0800

    Added the todo app, it run smoothly

---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+export function NewTodoForm({ addTodo }) {
+  const [newItem, setNewItem] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (newItem === "") return;
+
+    addTodo(newItem);
+
+    setNewItem("");
+  }
+  return (
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item"> New Input</label>
+        <input
+          value={newItem}
+          onInput={(e) => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  );
+}
